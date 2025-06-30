8d64cf435918854c5bcc05073cabe94bc8f1a696
Parmeet <parmeet202005@gmail.com>
2025-06-30 11:59:45 +0530

    changed some config

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::str::FromStr;
 async fn main() {
     let app = Router::new().route("/balance/{publickey}", get(get_balance_handler));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
         .await
         .unwrap();
 
