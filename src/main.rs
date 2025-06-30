4f02c14a042c7b3be5d2191a5765463bdec160d4
Parmeet <parmeet202005@gmail.com>
2025-06-30 14:50:23 +0530

    added few improvements

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use axum::{extract::Path, routing::get, Router};
+use axum::{Json, Router, extract::Path, routing::get};
+use serde::{Deserialize, Serialize};
 use solana_client::nonblocking::rpc_client::RpcClient;
 use solana_sdk::pubkey::Pubkey;
 use std::str::FromStr;
@@ -7,21 +8,40 @@ use std::str::FromStr;
 async fn main() {
     let app = Router::new().route("/balance/{publickey}", get(get_balance_handler));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn get_balance_handler(Path(publickey): Path<String>) -> String {
+#[derive(Serialize, Deserialize)]
+struct BalanceResp {
+    balance: f64,
+    success: bool,
+    message: String,
+}
+
+async fn get_balance_handler(Path(publickey): Path<String>) -> Json<BalanceResp> {
     let client = RpcClient::new("https://api.devnet.solana.com".to_string());
     let key = match Pubkey::from_str(&publickey) {
         Ok(key) => key,
-        Err(_) => return "Invalid public key".to_string(),
+        Err(e) => {
+            return Json::from(BalanceResp {
+                balance: 0.0,
+                success: false,
+                message: format!("Invalid public key: {}", e),
+            });
+        }
     };
     match client.get_balance(&key).await {
-        Ok(balance) => format!("balance is: {} SOL", balance as f64 / 1_000_000_000.0),
-        Err(e) => format!("Error: {}", e),
+        Ok(balance) => Json::from(BalanceResp {
+            balance: balance as f64 / 1_000_000_000.0,
+            success: true,
+            message: format!("success"),
+        }),
+        Err(e) => Json::from(BalanceResp {
+            balance: 0.0,
+            success: false,
+            message: format!("Error fetching balance: {}", e),
+        }),
     }
 }
